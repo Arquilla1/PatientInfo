126ca9d858c03b87497adebdc6f7693357d167bd
Arquilla1 <Arquilla1@msn.com>
2020-04-14 14:30:31 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# PatientInfo" 
+"# Patient Info" 
---
+++ src/PatientInfo.java
@@ -0,0 +1,27 @@
+public class PatientInfo {
+
+    public static class Main {
+
+        public static void main(String[] args) {
+
+            String patientName = "Stacey Manson";
+            boolean hasInsurance = false;
+            int temp = 102;
+            double numVisits = 5.02;
+            int patientcopay = 50;
+            String nextappointment = "Monday" + "Tuesday + Wednesday" + "Thursday" + "Friday" ;
+            String contactnumber = "555-555-9999";
+
+
+            System.out.println("Patient Details:");
+            System.out.println("Name: " + patientName);
+            System.out.println("Patient has insurance: " + hasInsurance);
+            System.out.println("Body temprature: " + temp);
+            System.out.println("Patient Copay: "  + patientcopay);
+            System.out.println("Next Appointment: " + nextappointment);
+            System.out.println("Contact Number: " + contactnumber);
+
+
+
+                    }
+    }}
